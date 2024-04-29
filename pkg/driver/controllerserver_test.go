7e9e67a783b4d00edb4b93748c0be0b3ca91664d
bhagyashree <bhagyak1@in.ibm.com>
2024-04-29 16:27:33 +0530

    fix travis issues

--- pkg/driver/controllerserver_test.go
+++ pkg/driver/controllerserver_test.go
@@ -21,16 +21,12 @@ import (
 	"reflect"
 	"testing"
 
-	fakeclient "github.com/IBM/ibm-object-csi-driver/pkg/driver/fake/s3client"
 	"github.com/IBM/ibm-object-csi-driver/pkg/s3client"
-	providerError "github.com/IBM/ibmcloud-storage-volume-lib/lib/utils"
-	"github.com/IBM/ibmcloud-volume-interface/lib/provider"
 	csi "github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func getCustomControllerServer(csiDriver *S3Driver, factory s3client.ObjectStorageSessionFactory, logger *zap.Logger) *controllerServer {
