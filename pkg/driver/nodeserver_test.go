96b5d8e9724c9273b719108ce069f992d42ef5fb
bhagyashree <bhagyak1@in.ibm.com>
2024-05-07 15:35:22 +0530

    fix linters

--- pkg/driver/nodeserver_test.go
+++ pkg/driver/nodeserver_test.go
@@ -18,10 +18,11 @@
 package driver
 
 import (
+	"testing"
+
 	csi "github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
-	"testing"
 )
 
 func TestNodeGetCapabilities(t *testing.T) {
