d16c376476881791d1094a8814281034f536c484
Mayank Sachan <34068620+mssachan@users.noreply.github.com>
2025-04-29 18:30:47 +0530

    Add validation for s3fs parameters (#164)
    * Add validation for s3fs parameters
    
    * Add validation for s3fs & rclone args

--- cos-csi-mounter/server/s3fs.go
+++ cos-csi-mounter/server/s3fs.go
@@ -27,7 +27,7 @@ type S3FSArgs struct {
 	MaxDirtyData            string `json:"max_dirty_data,omitempty"`
 	MaxStatCacheSize        string `json:"max_stat_cache_size,omitempty"`
 	MPUmask                 string `json:"mp_umask,omitempty"`
-	MultipartSize           string `json:"multipart_size,omitempty"`
+	MultiPartSize           string `json:"multipart_size,omitempty"`
 	MultiReqMax             string `json:"multireq_max,omitempty"`
 	ParallelCount           string `json:"parallel_count,omitempty"`
 	PasswdFilePath          string `json:"passwd_file,omitempty"`
@@ -75,14 +75,225 @@ func (args S3FSArgs) Validate(targetPath string) error {
 		return err
 	}
 
-	retryCount, err := strconv.Atoi(args.RetryCount)
-	if err != nil {
-		logger.Error("cannot convert value of retires into integer", zap.Error(err))
-		return fmt.Errorf("Cannot convert value of retires into integer: %v", err)
+	// Check if value of allow_other parameter is boolean "true" or "false"
+	if args.AllowOther != "" {
+		if isBool := isBoolString(args.AllowOther); !isBool {
+			logger.Error("cannot convert value of allow_other into boolean", zap.Any("allow_other", args.AllowOther))
+			return fmt.Errorf("cannot convert value of allow_other into boolean: %v", args.AllowOther)
+		}
 	}
-	if retryCount < 1 {
-		logger.Error("value of retires should be >= 1", zap.Error(err))
-		return fmt.Errorf("value of retires should be >= 1")
+
+	// Check if value of auto_cache parameter is boolean "true" or "false"
+	if args.AutoCache != "" {
+		if isBool := isBoolString(args.AutoCache); !isBool {
+			logger.Error("cannot convert value of auto_cache into boolean", zap.Any("auto_cache", args.AutoCache))
+			return fmt.Errorf("cannot convert value of auto_cache into boolean: %v", args.AutoCache)
+		}
 	}
+
+	// Check if value of connect_timeout parameter can be converted to integer
+	if args.ConnectTimeoutSeconds != "" {
+		_, err := strconv.Atoi(args.ConnectTimeoutSeconds)
+		if err != nil {
+			logger.Error("cannot convert value of connect_timeout into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of connect_timeout into integer: %v", err)
+		}
+	}
+
+	// Check if value of curldbg parameter is either "body" or "normal"
+	if args.CurlDebug != "" && !(args.CurlDebug == "body" || args.CurlDebug == "normal") {
+		logger.Error("invalid value for 'curldbg' param. Should be either 'body' or 'normal'", zap.Any("curldbg", args.CurlDebug))
+		return fmt.Errorf("invalid value for 'curldbg' param. Should be either 'body' or 'normal': %v", args.CurlDebug)
+	}
+
+	if !(strings.HasPrefix(args.EndPoint, "https://") || strings.HasPrefix(args.EndPoint, "http://")) {
+		logger.Error("bad value for endpoint: scheme is missing."+
+			" Must be of the form http://<hostname> or https://<hostname>",
+			zap.String("endpoint", args.EndPoint))
+		return fmt.Errorf("bad value for endpoint \"%v\": scheme is missing."+
+			" Must be of the form http://<hostname> or https://<hostname>", args.EndPoint)
+	}
+
+	// Check if value of gid parameter can be converted to integer
+	if args.GID != "" {
+		_, err := strconv.Atoi(args.GID)
+		if err != nil {
+			logger.Error("cannot convert value of gid into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of gid into integer: %v", err)
+		}
+	}
+
+	// Check if value of ibm_iam_auth parameter is boolean "true" or "false"
+	if args.IBMIamAuth != "" {
+		if isBool := isBoolString(args.IBMIamAuth); !isBool {
+			logger.Error("cannot convert value of ibm_iam_auth into boolean", zap.Any("ibm_iam_auth", args.IBMIamAuth))
+			return fmt.Errorf("cannot convert value of ibm_iam_auth into boolean: %v", args.IBMIamAuth)
+		}
+	}
+
+	if args.IBMIamEndpoint != "" {
+		if !(strings.HasPrefix(args.IBMIamEndpoint, "https://") || strings.HasPrefix(args.IBMIamEndpoint, "http://")) {
+			logger.Error("bad value for ibm_iam_endpoint."+
+				" Must be of the form https://<hostname> or http://<hostname>",
+				zap.String("ibm_iam_endpoint", args.IBMIamEndpoint))
+			return fmt.Errorf("bad value for ibm_iam_endpoint \"%v\":"+
+				" Must be of the form https://<hostname> or http://<hostname>", args.IBMIamEndpoint)
+		}
+	}
+
+	// Check if value of kernel_cache parameter is boolean "true" or "false"
+	if args.KernelCache != "" {
+		if isBool := isBoolString(args.KernelCache); !isBool {
+			logger.Error("cannot convert value of kernel_cache into boolean", zap.Any("kernel_cache", args.KernelCache))
+			return fmt.Errorf("cannot convert value of kernel_cache into boolean: %v", args.KernelCache)
+		}
+	}
+
+	// Check if value of max_background parameter can be converted to integer
+	if args.MaxBackground != "" {
+		_, err := strconv.Atoi(args.MaxBackground)
+		if err != nil {
+			logger.Error("cannot convert value of max_background into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of max_background into integer: %v", err)
+		}
+	}
+
+	// Check if value of max_dirty_data parameter can be converted to integer
+	if args.MaxDirtyData != "" {
+		_, err := strconv.Atoi(args.MaxDirtyData)
+		if err != nil {
+			logger.Error("cannot convert value of max_dirty_data into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of max_dirty_data into integer: %v", err)
+		}
+	}
+
+	// Check if value of max_stat_cache_size parameter can be converted to integer
+	if args.MaxStatCacheSize != "" {
+		_, err := strconv.Atoi(args.MaxStatCacheSize)
+		if err != nil {
+			logger.Error("cannot convert value of max_stat_cache_size into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of max_stat_cache_size into integer: %v", err)
+		}
+	}
+
+	// Check if value of multipart_size parameter can be converted to integer
+	if args.MultiPartSize != "" {
+		_, err := strconv.Atoi(args.MultiPartSize)
+		if err != nil {
+			logger.Error("cannot convert value of multipart_size into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of multipart_size into integer: %v", err)
+		}
+	}
+
+	// Check if value of multireq_max parameter can be converted to integer
+	if args.MultiReqMax != "" {
+		_, err := strconv.Atoi(args.MultiReqMax)
+		if err != nil {
+			logger.Error("cannot convert value of multireq_max into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of multireq_max into integer: %v", err)
+		}
+	}
+
+	// Check if value of parallel_count parameter can be converted to integer
+	if args.ParallelCount != "" {
+		_, err := strconv.Atoi(args.ParallelCount)
+		if err != nil {
+			logger.Error("cannot convert value of parallel_count into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of parallel_count into integer: %v", err)
+		}
+	}
+
+	// Check if .passwd file exists or not
+	if exists, err := fileExists(args.PasswdFilePath); err != nil {
+		logger.Error("error checking file existence", zap.Error(err), zap.Any("path", args.PasswdFilePath))
+		return fmt.Errorf("error checking file existence: %v", err)
+	} else if !exists {
+		logger.Error("file not found", zap.Error(err), zap.Any("path", args.PasswdFilePath))
+		return fmt.Errorf("file not found: %v", args.PasswdFilePath)
+	}
+
+	// Check if value of ro parameter is boolean "true" or "false"
+	if args.ReadOnly != "" {
+		if isBool := isBoolString(args.ReadOnly); !isBool {
+			logger.Error("cannot convert value of ro into boolean", zap.Any("ro", args.ReadOnly))
+			return fmt.Errorf("cannot convert value of roe into boolean: %v", args.ReadOnly)
+		}
+	}
+
+	// Check if value of readwrite_timeout parameter can be converted to integer
+	if args.ReadwriteTimeoutSeconds != "" {
+		_, err := strconv.Atoi(args.ReadwriteTimeoutSeconds)
+		if err != nil {
+			logger.Error("cannot convert value of readwrite_timeout into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of readwrite_timeout into integer: %v", err)
+		}
+	}
+
+	// Check if value of retries parameter can be converted to integer
+	if args.RetryCount != "" {
+		retryCount, err := strconv.Atoi(args.RetryCount)
+		if err != nil {
+			logger.Error("cannot convert value of retires into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of retires into integer: %v", err)
+		}
+		if retryCount < 1 {
+			logger.Error("value of retires should be >= 1")
+			return fmt.Errorf("value of retires should be >= 1")
+		}
+	}
+
+	// Check if value of sigv2 parameter is boolean "true" or "false"
+	if args.SigV2 != "" {
+		if isBool := isBoolString(args.SigV2); !isBool {
+			logger.Error("cannot convert value of sigv2 into boolean", zap.Any("sigv2", args.SigV2))
+			return fmt.Errorf("cannot convert value of sigv2 into boolean: %v", args.SigV2)
+		}
+	}
+
+	// Check if value of sigv4 parameter is boolean "true" or "false"
+	if args.SigV4 != "" {
+		if isBool := isBoolString(args.SigV4); !isBool {
+			logger.Error("cannot convert value of sigv4 into boolean", zap.Any("sigv4", args.SigV4))
+			return fmt.Errorf("cannot convert value of sigv4 into boolean: %v", args.SigV4)
+		}
+	}
+
+	// Check if value of stat_cache_expire parameter can be converted to integer
+	if args.StatCacheExpireSeconds != "" {
+		cacheExpireSeconds, err := strconv.Atoi(args.StatCacheExpireSeconds)
+		if err != nil {
+			logger.Error("cannot convert value of stat_cache_expire into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of stat_cache_expire into integer: %v", err)
+		} else if cacheExpireSeconds < 0 {
+			logger.Error("value of stat_cache_expire should be >= 0")
+			return fmt.Errorf("value ofstat_cache_expire should be >= 0")
+		}
+	}
+
+	// Check if value of uid parameter can be converted to integer
+	if args.UID != "" {
+		_, err := strconv.Atoi(args.UID)
+		if err != nil {
+			logger.Error("cannot convert value of uid into integer", zap.Error(err))
+			return fmt.Errorf("cannot convert value of uid into integer: %v", err)
+		}
+	}
+
+	// Check if value of use_path_request_style parameter is boolean "true" or "false"
+	if args.UsePathRequestStyle != "" {
+		if isBool := isBoolString(args.UsePathRequestStyle); !isBool {
+			logger.Error("cannot convert value of use_path_request_style into boolean", zap.Any("use_path_request_style", args.UsePathRequestStyle))
+			return fmt.Errorf("cannot convert value of use_path_request_style into boolean: %v", args.UsePathRequestStyle)
+		}
+	}
+
+	// Check if value of use_xattr parameter is boolean "true" or "false"
+	if args.UseXattr != "" {
+		if isBool := isBoolString(args.UseXattr); !isBool {
+			logger.Error("cannot convert value of use_xattr into boolean", zap.Any("use_xattr", args.UseXattr))
+			return fmt.Errorf("cannot convert value of use_xattr into boolean: %v", args.UseXattr)
+		}
+	}
+
 	return nil
 }
--- cos-csi-mounter/server/utils.go
+++ cos-csi-mounter/server/utils.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -61,4 +62,22 @@ func (req *MountRequest) ParseMounterArgs() ([]string, error) {
 	default:
 		return nil, fmt.Errorf("unknown mounter: %s", req.Mounter)
 	}
+}
+
+// isBoolString checks if a string is "true" or "false" (case-insensitive)
+func isBoolString(s string) bool {
+	s = strings.TrimSpace(strings.ToLower(s))
+	return s == "true" || s == "false"
+}
+
+// fileExists checks whether the given file path exists and is not a directory.
+func fileExists(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return !info.IsDir(), nil
 }
