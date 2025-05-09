e18f1aa7756883844dd1a755c8bd0ff2ac58721e
Mayank Sachan <masachan@in.ibm.com>
2025-05-09 11:37:47 +0530

    publish deb pakcge by fixing issues with debain package generation

--- cos-csi-mounter/install/postinst.sh
+++ cos-csi-mounter/install/postinst.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
-
 # Once package is installed this script will called to enable the service
+
+# Reload systemd unit files
+systemctl daemon-reload || true
+
+echo "[postinst] Enabling service..."
 systemctl enable cos-csi-mounter.service
+
+echo "[postinst] Starting service..."
 systemctl start cos-csi-mounter.service
+
+echo "[postinst] Done."
