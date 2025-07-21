e9eabdea8ea074030ff5be79a34f24a5e94708c1
Ashima-Ashima1 <Ashima.Ashima1@ibm.com>
2025-07-21 17:49:18 +0530

    return response if non empty
    Signed-off-by: Ashima-Ashima1 <Ashima.Ashima1@ibm.com>

--- pkg/mounter/mounter-rclone.go
+++ pkg/mounter/mounter-rclone.go
@@ -15,6 +15,7 @@ import (
 	"bufio"
 	"crypto/sha256"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path"
@@ -213,6 +214,9 @@ func (rclone *RcloneMounter) Mount(source string, target string) error {
 		response, err := mounterRequest(payload, "http://unix/api/cos/mount")
 		klog.Info("Worker Mounting...", response)
 		if err != nil {
+			if strings.TrimSpace(response) != "" {
+				return errors.New(response)
+			}
 			return err
 		}
 		return nil
@@ -232,6 +236,9 @@ func (rclone *RcloneMounter) Unmount(target string) error {
 		response, err := mounterRequest(payload, "http://unix/api/cos/unmount")
 		klog.Info("Worker Unmounting...", response)
 		if err != nil {
+			if strings.TrimSpace(response) != "" {
+				return errors.New(response)
+			}
 			return err
 		}
 
