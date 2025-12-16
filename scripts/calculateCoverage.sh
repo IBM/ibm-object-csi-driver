ec9472fd48dbf789ebe99847e584542c5772ccf9
Prachi Shivanand Anure <prachishivanandanure@Prachis-MacBook-Pro.local>
2025-12-16 10:56:52 +0530

    minor changes

--- scripts/calculateCoverage.sh
+++ scripts/calculateCoverage.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #******************************************************************************
-# Copyright 2021 IBM Corp.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# * Licensed Materials - Property of IBM
+# * IBM Cloud Kubernetes Service, 5737-D43
+# * (C) Copyright IBM Corp. 2025 All Rights Reserved.
+# * US Government Users Restricted Rights - Use, duplication or
+# * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 #******************************************************************************
+#
+# This script calculates the test coverage from cover.html and outputs the percentage.
+#
+# It is called by the GitHub Action in the pipeline to calculate the coverage percentage.
+
+# Extract the coverage percentage from cover.html
 
 COVERAGE=$(cat cover.html | grep "%)" | sed 's/[][()><%]/ /g' | awk '{ print $4 }' | awk '{s+=$1}END{print s/NR}')
 echo "-------------------------------------------------------------------------"
